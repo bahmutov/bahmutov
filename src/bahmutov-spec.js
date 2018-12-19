ec5e2a4745c6d2381b68c56ce339f9a12633de25
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-19 16:38:57 -0500

    chore: initial

---
+++ src/bahmutov-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const bahmutov = require('.')
+
+describe('bahmutov', () => {
+  it('write this test', () => {
+    console.assert(bahmutov, 'should export something')
+  })
+})
